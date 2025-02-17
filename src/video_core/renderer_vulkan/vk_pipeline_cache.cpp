c5b5a7faf8fc020d374622339073c623b1378d72
lucashartmann <lucas.a111@hotmail.com>
2025-02-17 13:37:56 -0300

    +-

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -219,7 +219,7 @@ PipelineCache::PipelineCache(const Instance& instance_, Scheduler& scheduler_,
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static std::vector<u64> skip_hashes = {};
+    static std::vector<u64> skip_hashes = {0xffe52ec0324f7efc, 0xed062e23f2be50fb};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         // LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
